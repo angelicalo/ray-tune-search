72a863ba76000e74fe2104b0a2fad9ca4a6a5694
Darlinne Hubert Palo Soto <ra264955@lmcad-dl-4.ic.unicamp.br>
2023-08-30 14:49:04 -0300

    Attempt to retrieve csv

--- read_experiment_and_create_csv.py
+++ read_experiment_and_create_csv.py
@@ -5,8 +5,8 @@ from ray import tune
 
 # Read ray tune experiment results
 experiment_name = "umap_hyperparameters_on_kuhar.standartized_balanced_starting_with_30.60.90...300"
-path = os.path.join("../ray_results", experiment_name)
-restored_tuner = tune.Tuner.restore(experiment_name, trainable=None)
+path = os.path.join("../../ray_results", experiment_name)
+restored_tuner = tune.Tuner.restore(path=path, trainable=None)
 result_grid = restored_tuner.get_results()
 results_df = pd.DataFrame(result_grid)
 results_df.to_csv('DATA_EXEMPLO.csv')
