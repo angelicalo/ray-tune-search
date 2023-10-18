e3aeace3bb8dfd6f069ab044fd8dce3cd2e44ba8
Hubert Palo <darlinnep@gmail.com>
2023-10-18 15:15:41 -0300

    Update hyperparameters_search.py

--- hyperparameters_search.py
+++ hyperparameters_search.py
@@ -129,7 +129,7 @@ def my_objective_function(
         syserror = sys.exc_info()
         # result = {'score': random.uniform(-20, -10)}
         result = {'score': -0.1, 'num_params': -1, 'num_trainable_params': -1, 'error_type': str(syserror[0]), 'error_message': str(syserror[1]), 'error_traceback': '\n'.join(traceback.format_tb(e.__traceback__))}
-        print('EXCEPTION'*10, result)
+        # print(result)
     session.report(result)
 
 
